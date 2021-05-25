c08fbccfed3494ef3d98201fa0b524d7c5bf9c3a
sunyu-pang <pangsunyu@gmail.com>
2021-05-25 13:44:37 +0900

    2.6 Log Out (fixed ver.)

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -10,7 +10,7 @@ export default () => {
     };
     return (
         <>
-            <button onCLick={onLogOutClick}>Log Out</button>
+            <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
  };
