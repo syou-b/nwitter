c044c7b89c49ee8ca9c2389d4f5a83fde0b5f505
sunyu-pang <pangsunyu@gmail.com>
2021-06-28 00:25:00 +0900

    #3.3 Realtime Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,26 @@
 import { dbService } from "fbase";
 import React, {useEffect, useState} from "react";
 
- const Home = () => {
+ const Home = ({userObj}) => {
      const [nweet, setNweet] = useState("");
      const [nweets, setNweets] = useState([]);
-     const getNweets = async() => {
-         const dbNweets =  await dbService.collection("nweets").get();
-         dbNweets.forEach((document) => {
-             const nweetObject = {
-                 ...document.data(),
-                 id: document.id,
-             } 
-             setNweets((prev) => [nweetObject, ...prev]);
-         });
-     }
+     
      useEffect(() => {
-        
+        getNweets();
+        dbService.collection("nweets").onSnapshot(snapshot => {
+            const nweetArray = snapshot.docs.map(doc => ({
+                id: doc.id, 
+                ...doc.data(),
+            }));
+            setNweets(nweetArray);
+        });
      }, [])
      const onSubmit = async (event) => {
          event.preventDefault();
          await dbService.collection("nweets").add({
-             nweet,
+             text: nweet,
              createAt :  Date.now(),
+             creatorId: userObj.uid,
          });
          setNweet("");
      };
@@ -39,7 +38,7 @@ import React, {useEffect, useState} from "react";
             </form>
             <div key={nweet.id}>
                 {nweets.map(nweet => <div>
-                    <h4>{nweet.nweet}</h4>
+                    <h4>{nweet.text}</h4>
                 </div>)}
             </div>
         </div>
