7d793cc4fa7949f9f4fa6a1a3e989035690f56f8
sunyu-pang <pangsunyu@gmail.com>
2021-06-29 15:56:43 +0900

    #5.2 Update Profile Bugfix

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,46 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
-import {authService} from "fbase";
+import { authService } from "fbase";
 
 function App() {
-  const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [userObj, setUserObj] = useState(null);
-  useEffect (() => {
-    authService.onAuthStateChanged((user) => {
-      if(user){
-        setUserObj(user);
-      }
-      setInit(true);
-    });
-  }, []);
-  return (
-    <>
-      {init? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
-      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
-    </>
-  );
+	const [init, setInit] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [userObj, setUserObj] = useState(null);
+	useEffect(() => {
+		authService.onAuthStateChanged((user) => {
+			if (user) {
+				setUserObj({
+					displayName: user.displayName,
+					uid: user.uid,
+					updateProfile: (args) => user.updateProfile(args),
+				});
+			}
+			setInit(true);
+		});
+	}, []);
+	const refreshUser = () => {
+		const user = authService.currentUser;
+		setUserObj({
+			displayName: user.displayName,
+			uid: user.uid,
+			updateProfile: (args) => user.updateProfile(args),
+		});
+	};
+
+	return (
+		<>
+			{init ? (
+				<AppRouter
+					refreshUser={refreshUser}
+					isLoggedIn={isLoggedIn}
+					userObj={userObj}
+				/>
+			) : (
+				"Initializing..."
+			)}
+			<footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+		</>
+	);
 }
 
 export default App;
